9693e127c0fda9fc631765339e51f4f6bb5140ee
Ho Xuan Vinh <hoxuanvinhuit93@gmail.com>
2016-09-01 23:39:21 +0700

    add edit account

---
+++ UDMSwift/ViewControllers/Settings/EdidAccountSettingViewController.swift
@@ -0,0 +1,11 @@
+//
+//  EdidAccountSettingViewController.swift
+//  UDMSwift
+//
+//  Created by OSXVN on 9/1/16.
+//  Copyright © 2016 XUANVINHTD. All rights reserved.
+//
+
+class EdidAccountSettingViewController: UITableViewController {
+
+}
